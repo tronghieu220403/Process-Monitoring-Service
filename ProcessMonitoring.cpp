8a2795fa60f6910a7cf9099e80013033c4586564
Nguyễn Trọng Hiếu <tronghieu220403@gmail.com>
2023-09-06 00:59:39 +0700

    Update ProcessMonitoring.cpp

--- ProcessMonitoring.cpp
+++ ProcessMonitoring.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
+#include <vector>
 
-#include "include/process/processsupervision.h"
+#ifdef _WIN32
+#include <Windows.h>
+#include <iphlpapi.h>
+#endif
+
+//#include "include/process/processsupervision.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
 
-    pm::ProcessSupervision p = pm::ProcessSupervision();
+    //auto p = pm::ProcessSupervision();
+
+    std::vector<unsigned char> buffer;
+    DWORD dwSize = sizeof(MIB_TCPTABLE_OWNER_PID);
+    DWORD dwRetValue = 0;
+    // repeat till buffer is big enough
+    do
+    {
+        buffer.resize(dwSize, 0);
+        dwRetValue = GetExtendedTcpTable(buffer.data(), &dwSize, TRUE, AF_INET, TCP_TABLE_OWNER_PID_ALL, 0);
+
+    } while (dwRetValue == ERROR_INSUFFICIENT_BUFFER);
+
 }
 
